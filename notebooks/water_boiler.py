5458f77121be0566a57c44e30f43fb6ddb1978e2
Ivo Georgiev <ivogeorg@gmail.com>
2021-09-27 19:24:14 -0600

    Tested notebooks
    Tested Jupyter notebooks on Windows

---
+++ notebooks/water_boiler.py
@@ -0,0 +1,18 @@
+class WaterBoiler:
+    """
+    Simple simulation of a water boiler which can heat up water
+    and where the heat dissipates slowly over time
+    """
+
+    def __init__(self):
+        self.water_temp = 20
+
+    def update(self, boiler_power, dt):
+        if boiler_power > 0:
+            # Boiler can only produce heat, not cold
+            self.water_temp += 1 * boiler_power * dt
+
+        # Some heat dissipation
+        self.water_temp -= 0.02 * dt
+        return self.water_temp
+    
